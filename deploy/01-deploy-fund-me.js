92207a4a1d16a79662222b377069391f64b456a6
Coin-Spark <spark@prospera.partners>
2022-09-28 19:01:45 -0300

    Initial commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,39 @@
+const { network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+// module.exports.default = deployFunc
+
+module.exports = async ({ getNameAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    // if chainId is X use address Y, etc, etc
+    // const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    let ethUsdPriceFeedAddress
+
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+    // if the contract doens't exist, lets deploy a minimal version of it
+    const args = [ethUsdPriceFeedAddress]
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: args, //pur priceFeedAddress
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
+    log("----------------------------------------------------------------")
+}
+module.exports.tags = ["all", "fundme"]
